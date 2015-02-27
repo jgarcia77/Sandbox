2223e40a7b0d33a9d0383fbbbb5377239cc60710
jgarcia77 <jgarcia773@gmail.com>
2015-02-27 13:34:20 -0600

    Console app for Guid generation

---
+++ Sandbox.GuidGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.GuidGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guid = Guid.NewGuid();
+
+            Console.WriteLine(guid);
+
+            guid = Guid.NewGuid();
+
+            Console.WriteLine(guid);
+
+            Console.Read();
+        }
+    }
+}
