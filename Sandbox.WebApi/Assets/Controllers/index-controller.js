4e491cb8b5b2f826ae5472c0ae34af44a59dc8f5
jgarcia77 <jgarcia773@gmail.com>
2015-03-04 12:57:57 -0600

    Infinite scrolling

--- Sandbox.WebApi/Assets/Controllers/index-controller.js
+++ Sandbox.WebApi/Assets/Controllers/index-controller.js
@@ -2,15 +2,55 @@
 
     var angularApp = angular.module("SandBox");
 
-    angularApp.controller("indexController", ["$scope", "$http", "$upload", "settings", indexController]);
-        
-    function indexController($scope, $http, $upload, settings) {
+    angularApp.controller("indexController", ["$window", "$scope", "$http", "$upload", "settings", indexController]);
+    
+    
 
+    function indexController($window, $scope, $http, $upload, settings) {
+
+        $scope.scrollTop = 0;
+        $scope.windowHeight = 0;
+        $scope.documentHeight = 0;
+
+        var windowElement = $(window);
+        var documentElement = document;
+
+        function getDocHeight() {
+            var height = Math.max(documentElement.body.scrollHeight, documentElement.documentElement.scrollHeight,
+                                  documentElement.body.offsetHeight, documentElement.documentElement.offsetHeight,
+                                  documentElement.body.clientHeight, documentElement.documentElement.clientHeight);
+
+            return height;
+        }
+
+        windowElement.on("scroll", function () {
+            $scope.scrollTop = windowElement.scrollTop();
+            $scope.windowHeight = windowElement.height();
+            //$scope.documentHeight = documentElement.height();
+
+            $scope.documentHeight = getDocHeight();
+
+            var result = ($scope.scrollTop + $scope.windowHeight > $scope.documentHeight - 100)
+
+            if (result) {
+                console.log("near bottom");
+            }
+
+        })
+
+        //$window.scroll(function () {
+            
+        //})
+
+        $scope.Files = null;
         
         $scope.upload = [];
         $scope.fileUploadObj = { testString1: "Test string 1", testString2: "Test string 2" };
 
         $scope.onFileSelect = function ($files) {
+
+            $scope.Files = $files;
+
             //$files: an array of files selected, each file has name, size, and type.
             for (var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
