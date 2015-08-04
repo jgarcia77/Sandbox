b44294f971f5487e0f5802cab70d6eabd39df1ee
jgarcia77 <jgarcia773@gmail.com>
2015-08-03 20:20:41 -0500

    ImageHelper

---
+++ Sandbox.Common/ImageHelper.cs
@@ -0,0 +1,210 @@
+﻿namespace Sandbox.Common
+{
+    using System.Drawing;
+    using System.IO;
+    using System.Drawing.Imaging;
+    using System;
+
+    public enum ImageCompareResults { Equal, Smaller, Bigger, NotDetermined }
+
+    public class ImageHelper
+    {
+        public string FileName { get; private set; }
+        public bool ImageExists { get; private set; }
+        public Image Image { get; private set; }
+        public string DirectoryName { get; private set; }
+        public string FileNameWithoutExtension { get; private set; }
+        public string Extension { get; private set; }
+        public ImageFormat ImageFormat
+        {
+            get
+            {
+                ImageFormat returnValue = null;
+
+                switch (this.Extension)
+                { 
+                    case ".bmp":
+                        returnValue = ImageFormat.Bmp;
+                        break;
+                    
+                    case ".emf":
+                        returnValue = ImageFormat.Emf;
+                        break;
+
+                    case ".exif":
+                        returnValue = ImageFormat.Exif;
+                        break;
+
+                    case ".gif":
+                        returnValue = ImageFormat.Gif;
+                        break;
+
+                    case ".ico":
+                        returnValue = ImageFormat.Icon;
+                        break;
+
+                    case ".jpeg":
+                    case ".jpg":
+                        returnValue = ImageFormat.Jpeg;
+                        break;
+
+                    case ".dmp":
+                        returnValue = ImageFormat.MemoryBmp;
+                        break;
+
+                    case ".png":
+                        returnValue = ImageFormat.Png;
+                        break;
+
+                    case ".tiff":
+                        returnValue = ImageFormat.Tiff;
+                        break;
+
+                    case ".wmf":
+                        returnValue = ImageFormat.Wmf;
+                        break;
+                }
+
+                return returnValue;
+            }
+        }
+
+        public string ToExension(ImageFormat imageFormat)
+        {
+            var returnValue = string.Empty;
+
+            if (imageFormat == ImageFormat.Bmp)
+            {
+                returnValue = ".bmp";
+            }
+            else if (imageFormat == ImageFormat.Emf)
+            {
+                returnValue = ".emf";
+            }
+            else if (imageFormat == ImageFormat.Exif)
+            {
+                returnValue = ".exif";
+            }
+            else if (imageFormat == ImageFormat.Gif)
+            {
+                returnValue = ".gif";
+            }
+            else if (imageFormat == ImageFormat.Icon)
+            {
+                returnValue = ".ico";
+            }
+            else if (imageFormat == ImageFormat.Jpeg)
+            {
+                returnValue = ".jpg";
+            }
+            else if (imageFormat == ImageFormat.MemoryBmp)
+            {
+                returnValue = ".dmp";
+            }
+            else if (imageFormat == ImageFormat.Png)
+            {
+                returnValue = ".png";
+            }
+            else if (imageFormat == ImageFormat.Tiff)
+            {
+                returnValue = ".tiff";
+            }
+            else if (imageFormat == ImageFormat.Wmf)
+            {
+                returnValue = ".wmf";
+            }
+
+            return returnValue;
+        }
+
+        public ImageHelper(string fileName)
+        {
+            this.FileName = fileName;
+
+            if (File.Exists(this.FileName))
+            {
+                this.Initialize();
+                this.ImageExists = true;
+            }
+            else
+            {
+                this.ImageExists = false;
+            }
+        }
+
+        private void Initialize()
+        {
+            this.Image = Image.FromFile(this.FileName);
+            this.DirectoryName = Path.GetDirectoryName(this.FileName);
+            this.FileNameWithoutExtension = Path.GetFileNameWithoutExtension(this.FileName);
+            this.Extension = Path.GetExtension(this.FileName);
+
+        }
+
+        public ImageCompareResults Compare(Quadrilateral quadrilateral)
+        {
+            ImageCompareResults returnValue = ImageCompareResults.NotDetermined;
+
+            if (this.Image.Width == quadrilateral.Width && this.Image.Height == quadrilateral.Height)
+            {
+                returnValue = ImageCompareResults.Equal;
+            }
+            else if (this.Image.Width < quadrilateral.Width || this.Image.Height < quadrilateral.Height)
+            {
+                returnValue = ImageCompareResults.Smaller;
+            }
+            else if (this.Image.Width > quadrilateral.Width || this.Image.Height > quadrilateral.Height)
+            {
+                returnValue = ImageCompareResults.Bigger;
+            }
+            
+            return returnValue;
+        }
+
+        public void ScaleImage(string destination, string fileName, Quadrilateral quadrilateral, ImageFormat imageFormatOverride = null)
+        {
+            var ratioX = (double)quadrilateral.Width / this.Image.Width;
+            var ratioY = (double)quadrilateral.Height / this.Image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(this.Image.Width * ratio);
+            var newHeight = (int)(this.Image.Height * ratio);
+
+            var newQuadrilateral = new Quadrilateral(newWidth, newHeight);
+
+            this.ResizeImage(destination, fileName, newQuadrilateral, imageFormatOverride);
+        }
+
+        public void ResizeImage(string destination, string fileName, Quadrilateral quadrilateral, ImageFormat imageFormatOverride = null)
+        {
+            var newImage = new Bitmap(quadrilateral.Width, quadrilateral.Height);
+
+            Graphics.FromImage(newImage)
+                    .DrawImage(this.Image, 0, 0, quadrilateral.Width, quadrilateral.Height);
+
+            if (imageFormatOverride == null)
+            {
+                var fileNameNew = string.Concat(destination, "//", fileName, this.Extension);
+                newImage.Save(fileNameNew, this.ImageFormat);
+            }
+            else
+            {
+                var extension = this.ToExension(imageFormatOverride);
+                var fileNameNew = string.Concat(destination, "//", fileName, extension);
+                newImage.Save(fileNameNew, imageFormatOverride);
+            }
+        }
+    }
+
+    public struct Quadrilateral
+    {
+        public int Width;
+        public int Height;
+
+        public Quadrilateral(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
--- Sandbox.ImageResizer/Program.cs
+++ Sandbox.ImageResizer/Program.cs
@@ -6,12 +6,129 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
 using System.Drawing.Imaging;
+using Sandbox.Common;
 
 namespace Sandbox.ImageResizer
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //ImageHelperConstructorTestFail();
+            //ImageHelperConstructorTestPass();
+            //ImageHelper_CompareResults();
+            //ImageHelper_Resize();
+            //ImageHelper_Resize_Shrink();
+            ImageHelper_Resize_Shrink_ToPng();
+
+            Console.Read();
+        }
+
+        private static void ImageHelperConstructorTestFail()
+        {
+            var fileName = @"C:\Users\JosueG\Desktop\Images\jordan.jpeg";
+            var helper = new ImageHelper(fileName);
+
+            Console.WriteLine(helper.ImageExists);
+        }
+
+        private static void ImageHelperConstructorTestPass()
+        { 
+            var fileName = @"C:\ProgramData\DirectiveBoards\Uploads\Images\Temp\avatar.png";
+            var helper = new ImageHelper(fileName);
+            
+            Console.WriteLine(helper.ImageExists);
+
+            var dimensions = "Width = {0}; Height = {1}";
+
+            Console.WriteLine(string.Format(dimensions, helper.Image.Width, helper.Image.Height));
+        }
+
+        private static void ImageHelper_CompareResults()
+        {
+            var fileName = @"C:\ProgramData\DirectiveBoards\Uploads\Images\Temp\avatar.png";
+            var helper = new ImageHelper(fileName);
+
+            var quadrilateral = new Quadrilateral(45, 45);
+            var result = helper.Compare(quadrilateral);
+
+            OutputImageCompare(helper.Image, quadrilateral, result);
+
+            quadrilateral = new Quadrilateral(90, 25);
+            result = helper.Compare(quadrilateral);
+
+            OutputImageCompare(helper.Image, quadrilateral, result);
+
+            quadrilateral = new Quadrilateral(25, 90);
+            result = helper.Compare(quadrilateral);
+
+            OutputImageCompare(helper.Image, quadrilateral, result);
+
+            quadrilateral = new Quadrilateral(25, 90);
+            result = helper.Compare(quadrilateral);
+
+            OutputImageCompare(helper.Image, quadrilateral, result);
+
+            quadrilateral = new Quadrilateral(25, 25);
+            result = helper.Compare(quadrilateral);
+
+            OutputImageCompare(helper.Image, quadrilateral, result);
+        }
+
+        private static void OutputImageCompare(Image image, Quadrilateral quadrilateral, ImageCompareResults result)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Result: {0}", result);
+            Console.WriteLine("Image Width: {0}", image.Width);
+            Console.WriteLine("Image Height: {0}", image.Height);
+            Console.WriteLine("New Width: {0}", quadrilateral.Width);
+            Console.WriteLine("New Height: {0}", quadrilateral.Height);
+        }
+
+        private static void ImageHelper_Resize()
+        {
+            var fileName = @"C:\ProgramData\DirectiveBoards\Uploads\Images\Temp\avatar.png";
+            var helper = new ImageHelper(fileName);
+
+            var quadrilateral = new Quadrilateral(90, 90);
+            helper.ScaleImage(helper.DirectoryName, "avatar_s90_90", quadrilateral);
+            helper.ResizeImage(helper.DirectoryName, "avatar_r90_90", quadrilateral);
+
+            quadrilateral = new Quadrilateral(90, 25);
+            helper.ScaleImage(helper.DirectoryName, "avatar_s90_25", quadrilateral);
+            helper.ResizeImage(helper.DirectoryName, "avatar_r90_25", quadrilateral);
+
+            quadrilateral = new Quadrilateral(25, 90);
+            helper.ScaleImage(helper.DirectoryName, "avatar_s25_90", quadrilateral);
+            helper.ResizeImage(helper.DirectoryName, "avatar_r25_90", quadrilateral);
+
+            quadrilateral = new Quadrilateral(90, 180);
+            helper.ScaleImage(helper.DirectoryName, "avatar_s90_180", quadrilateral);
+            helper.ResizeImage(helper.DirectoryName, "avatar_r90_180", quadrilateral);
+            
+        }
+
+        private static void ImageHelper_Resize_Shrink()
+        {
+            var fileName = @"C:\ProgramData\DirectiveBoards\Uploads\Images\Temp\oberon.jpg";
+            var helper = new ImageHelper(fileName);
+
+            var quadrilateral = new Quadrilateral(45, 45);
+            helper.ScaleImage(helper.DirectoryName, "oberon_s45_45", quadrilateral);
+            helper.ResizeImage(helper.DirectoryName, "oberon_r45_45", quadrilateral);
+        }
+
+        private static void ImageHelper_Resize_Shrink_ToPng()
+        {
+            var fileName = @"C:\ProgramData\DirectiveBoards\Uploads\Images\Temp\oberon.jpg";
+            var helper = new ImageHelper(fileName);
+
+            var quadrilateral = new Quadrilateral(45, 45);
+            helper.ScaleImage(helper.DirectoryName, "oberon_s45_45", quadrilateral, ImageFormat.Png);
+            helper.ResizeImage(helper.DirectoryName, "oberon_r45_45", quadrilateral, ImageFormat.Png);
+        }
+
+        public static void ResizeImage()
         {
             var file = @"C:\Users\JosueG\Desktop\Images\jordan.jpeg";
 
