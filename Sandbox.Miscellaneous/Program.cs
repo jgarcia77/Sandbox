61b2a0930459dd8a208f670126ee38a46c5430ed
jgarcia77 <jgarcia773@gmail.com>
2017-02-20 14:45:36 -0600

    Round to significant digits

--- Sandbox.Miscellaneous/Program.cs
+++ Sandbox.Miscellaneous/Program.cs
@@ -10,13 +10,55 @@ namespace Sandbox.Miscellaneous
     {
         static void Main(string[] args)
         {
-            int dataTypeId = 2;
-
-            var dataType = (DataTypes)dataTypeId;
-
-            var dataTypeId2 = (int)dataType;
+            NumberFormatting();
         }
-    }
 
-    enum DataTypes { DateTime, String, Boolean }
+        static void NumberFormatting()
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+
+                var input = Console.ReadLine();
+
+                try
+                {
+                    var inputDouble = Convert.ToDouble(input);
+
+                    var roundDouble = TruncateToSignificantDigits(inputDouble, 4);
+
+                    Console.WriteLine(roundDouble);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        static double TruncateToSignificantDigits(double d, int digits)
+        {
+            if (d == 0.0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                double leftSideNumbers = Math.Floor(Math.Log10(Math.Abs(d))) + 1;
+                double scale = Math.Pow(10, leftSideNumbers);
+                double result = scale * Math.Round(d / scale, digits, MidpointRounding.AwayFromZero);
+
+                // Clean possible precision error.
+                if ((int)leftSideNumbers >= digits)
+                {
+                    return Math.Round(result, 0, MidpointRounding.AwayFromZero);
+                }
+                else
+                {
+                    return Math.Round(result, digits - (int)leftSideNumbers, MidpointRounding.AwayFromZero);
+                }
+            }
+        }
+
+    }
 }
