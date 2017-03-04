526c1d3c3b5648b7c151122d329a7ba7053b67b0
jgarcia77 <jgarcia773@gmail.com>
2017-03-04 08:40:54 -0600

    Open XML project setup

---
+++ Sandbox.OpenXML/HyperlinkExample.cs
@@ -0,0 +1,76 @@
+﻿namespace Sandbox.OpenXML
+{
+    using DocumentFormat.OpenXml;
+    using DocumentFormat.OpenXml.Packaging;
+    using DocumentFormat.OpenXml.Spreadsheet;
+
+    public static class HyperlinkExample
+    {
+        private static SpreadsheetDocument document;
+        private static WorkbookPart workbookPart;
+        private static Sheets sheets;
+
+        public static void Create(string path)
+        {
+            document = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook);
+
+            InitWorkbookPart();
+
+            AddWorksheet();
+
+            document.Close();
+        }
+
+        private static void InitWorkbookPart()
+        {
+            workbookPart = document.AddWorkbookPart();
+
+            workbookPart.Workbook = new Workbook();
+
+            sheets = document.WorkbookPart.Workbook.AppendChild(new Sheets());
+        }
+
+        private static void AddWorksheet()
+        {
+            var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+
+            var worksheet = new Worksheet();
+
+            var sheetData = new SheetData();
+
+            FillSheetData(sheetData);
+
+            worksheet.Append(sheetData);
+
+            worksheetPart.Worksheet = worksheet;
+
+            worksheetPart.Worksheet.Save();
+
+            var sheet = new Sheet
+            {
+                Id = document.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = (uint)1,
+                Name = "HyperlinkExample"
+            };
+
+            sheets.Append(sheet);
+
+            document.WorkbookPart.Workbook.Save();
+        }
+
+        private static void FillSheetData(SheetData sheetData)
+        {
+            var row = new Row();
+
+            var cell = new Cell
+            {
+                DataType = CellValues.String,
+                CellValue = new CellValue("This is example of adding hyperlinks to a worksheet.")
+            };
+
+            row.Append(cell);
+
+            sheetData.Append(row);
+        }
+    }
+}
---
+++ Sandbox.OpenXML/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox.OpenXML
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHyperlinkExample(Environment.CurrentDirectory);
+        }
+
+        private static void CreateHyperlinkExample(string directory)
+        {
+            var path = Path.Combine(directory, "HyperlinkExample.xlsx");
+
+            HyperlinkExample.Create(path);
+        }
+    }
+}
